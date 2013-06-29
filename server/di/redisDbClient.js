169a3c80c792a355a5d489a9297e1672f52e2270
Subak Systems <info@subak.jp>
2013-06-29 11:52:04 +0900

    heroku対応

--- server/di/redisDbClient.js
+++ server/di/redisDbClient.js
@@ -1,5 +1,13 @@
-define(["redis"], function (redis) {
-  var client = redis.createClient();
+define(["url", "redis"], function (url, redis) {
+  var client, rtg;
+
+  if (process.env.REDISTOGO_URL) {
+    rtg = url.parse(process.env.REDISTOGO_URL);
+    client = redis.createClient(rtg.port, rtg.hostname);
+    client.auth(rtg.auth.split(":")[1]);
+  } else {
+    client = redis.createClient();
+  }
 
   return client;
 });
